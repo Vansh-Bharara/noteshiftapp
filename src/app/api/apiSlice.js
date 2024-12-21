f41fb485b22a8c66922f2164124c2aec79b3cd4c
Vanssh Barara <vansshbarara@Vansshs-MacBook-Air.local>
2024-12-21 14:43:34 +0530

    updated the baseurl according to render

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://noteshiftapp.onrender.com',
+    baseUrl: 'https://noteshiftappapi.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
